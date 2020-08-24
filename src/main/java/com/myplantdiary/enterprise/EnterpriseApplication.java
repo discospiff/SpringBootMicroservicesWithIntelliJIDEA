5cc2ada4afc1a8d3b2cbfa26ff8c66b84adc9e61
Brandan Jones <Brandan.Jones@uc.edu>
2020-08-24 18:56:34 -0400

    Initial commit

---
+++ src/main/java/com/myplantdiary/enterprise/EnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.myplantdiary.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
@@ -0,0 +1,18 @@
+package com.myplantdiary.enterprise;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PlantDiaryController {
+
+    /**
+     * Handle the root (/) endpoint and return a start page.
+     * @return
+     */
+    @RequestMapping("/")
+    public String index() {
+        return "start";
+    }
+
+}
