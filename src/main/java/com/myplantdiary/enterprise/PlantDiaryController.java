3be7f38777c581b74aaa4b9e591c1b1c6068bd9d
Brandan Jones <Brandan.Jones@uc.edu>
2020-09-13 12:37:28 -0400

    Add new CRD endpoints.

--- src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
+++ src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
@@ -1,8 +1,11 @@
 package com.myplantdiary.enterprise;
 
 import com.myplantdiary.enterprise.dto.Specimen;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class PlantDiaryController {
@@ -13,10 +16,28 @@ public class PlantDiaryController {
      */
     @RequestMapping("/")
     public String index() {
-        Specimen specimen = new Specimen();
-        specimen.setDescription("Foo");
-        String desc = specimen.getDescription();
         return "start";
     }
+
+    @GetMapping("/specimen")
+    public ResponseEntity fetchAllSpecimens() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/specimen/{id}/")
+    public ResponseEntity fetchSpecimenById(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/specimen", consumes="application/json", produces="application/json")
+    public Specimen createSpecimen(@RequestBody Specimen specimen) {
+        return specimen;
+    }
+
+    @DeleteMapping("/specimen/{id}/")
+    public ResponseEntity deleteSpecimen(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
 
 }
