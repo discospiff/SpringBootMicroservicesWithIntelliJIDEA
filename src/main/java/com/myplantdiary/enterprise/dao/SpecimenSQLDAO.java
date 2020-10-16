007e9c419bc7796b58a8cbcc9019db4c89fb37d1
Brandan Jones <Brandan.Jones@uc.edu>
2020-10-15 21:13:06 -0400

    Fix an odd pom issue; recursive reference.

--- src/main/java/com/myplantdiary/enterprise/dao/SpecimenSQLDAO.java
+++ src/main/java/com/myplantdiary/enterprise/dao/SpecimenSQLDAO.java
@@ -2,12 +2,14 @@ package com.myplantdiary.enterprise.dao;
 
 import com.myplantdiary.enterprise.dto.Specimen;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository("specimenDAO")
+@Profile("test")
 public class SpecimenSQLDAO implements ISpecimenDAO {
 
     @Autowired
