e6e8d3622ec0e471584eb918f45d2b34d193829c
Brandan Jones <Brandan.Jones@uc.edu>
2020-10-15 21:58:42 -0400

    Add Profile annotations

--- src/main/java/com/myplantdiary/enterprise/dao/SpecimenRepository.java
+++ src/main/java/com/myplantdiary/enterprise/dao/SpecimenRepository.java
@@ -1,8 +1,10 @@
 package com.myplantdiary.enterprise.dao;
 
 import com.myplantdiary.enterprise.dto.Specimen;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 
+@Profile("!test")
 public interface SpecimenRepository extends CrudRepository<Specimen, Integer> {
 
 
