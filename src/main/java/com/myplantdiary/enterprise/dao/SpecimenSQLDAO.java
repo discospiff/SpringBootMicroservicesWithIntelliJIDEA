dac0a2bb8a2bfd09a3964b89bb9625ac6acc0047
Brandan Jones <Brandan.Jones@uc.edu>
2020-10-15 21:56:44 -0400

    Add Profile annotations

--- src/main/java/com/myplantdiary/enterprise/dao/SpecimenSQLDAO.java
+++ src/main/java/com/myplantdiary/enterprise/dao/SpecimenSQLDAO.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository("specimenDAO")
+@Repository
 @Profile("dev")
 public class SpecimenSQLDAO implements ISpecimenDAO {
 
